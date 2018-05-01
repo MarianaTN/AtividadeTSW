ef7a2b3329f3a56ea8ad5c340f090dc95df33a6a
Mariana Trevizani <mariana.trevizani@yahoo.com.br>
2018-05-01 17:50:49 -0300

    ajuste class de exemplo

--- src/test/java/CadastroGmailTest.java
+++ src/test/java/CadastroGmailTest.java
@@ -11,27 +11,13 @@ import static org.junit.Assert.*;
 public class CadastroGmailTest {
     private static WebDriver driver;
 
-   // @FindBy(name="q")
-    private WebElement buscarNoGoogle;
-
     @BeforeClass
     public static void configura() {
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\marit\\geckodriver\\geckodriver.exe");
         driver = new FirefoxDriver();
-        //driver.get("https://www.google.com.br/");
         driver.get("https://accounts.google.com/signup/v2/webcreateaccount?continue=https%3A%2F%2Faccounts.google.com%2FManageAccount&flowName=GlifWebSignIn&flowEntry=SignUp");
     }
-    @Test
-    public void busca() {
-        buscarNoGoogle = driver.findElement(By.name("q"));
-        this.buscarNoGoogle.sendKeys("Teste de software em microserviços");
-        this.buscarNoGoogle.click();
 
-        boolean achouTeste = driver.getPageSource().contains("Teste de software");
-        assertTrue(achouTeste);
-
-        driver.close();
-    }
 	@Test
 	public void test() {
 		CadastroGmail pag = PageFactory.initElements(driver, CadastroGmail.class);
