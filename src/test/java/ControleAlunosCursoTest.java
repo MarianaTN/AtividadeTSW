647bedd91fc7d82f15161462986d7ab99dfd6388
Aline Sotte <alinedpaso@gmail.com>
2018-05-12 01:52:08 -0300

    Cadastro de Alunos em Curso e Calendário

---
+++ src/test/java/ControleAlunosCursoTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertTrue;
+
+public class ControleAlunosCursoTest {
+@Test
+public void teste() {
+}}
