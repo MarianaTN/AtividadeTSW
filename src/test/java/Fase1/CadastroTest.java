c38a05e574908ceff66d730a89a6c90e1ccd2db8
Mariana Trevizani <mariana.trevizani@yahoo.com.br>
2018-05-16 15:57:01 -0300

    ajustes login, cadastro e forum

--- src/test/java/Fase1/CadastroTest.java
+++ src/test/java/Fase1/CadastroTest.java
@@ -1,5 +1,6 @@
 package Fase1;
 
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -9,12 +10,15 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class CadastroTest {
     private static WebDriver driver;
 
-    private String url = "https://atlantis.isti.cnr.it:5000/course";
+    private String urlLogado = "https://atlantis.isti.cnr.it:5000/course";
 
     @BeforeClass
     public static void configura() {
@@ -26,34 +30,44 @@ public class CadastroTest {
     }
 
     @Test
-    public void testCadastro() throws InterruptedException {
+    public void testCadastro() {
         Cadastro pag = PageFactory.initElements(driver, Cadastro.class);
-        pag.preencheDados("studentufjf@gmail.com", "Student Ufjf", "123456Ab","123456Ab");
+        pag.preencheDados("studentufjf@gmail.com","Student Ufjf", "123456Ab", "123456Ab");
 
-        driver.findElement(By.xpath("/html/body/app/div/main/app-presentation/login-modal/div/div[1]/div/form/div[5]/div/re-captcha/div/div/div/iframe")).click();
-        driver.findElement(By.id("disabled-signup-btn")).click();
-        boolean logado = new WebDriverWait(driver, 10).until(ExpectedConditions.urlMatches(url));
+        driver.findElement(By.xpath("/html/body/app/div/main/app-presentation/login-modal/div/div[1]/div/form/div[5]/div/re-captcha/div/div/div")).click();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.findElement(By.id("sign-up-btn")).click();
+        boolean logado = new WebDriverWait(driver, 10).until(ExpectedConditions.urlMatches(urlLogado));
 
         assertTrue(logado);
     }
 
     @Test
-    public void testCadastroInvalidoEmail() throws InterruptedException {
+    public void testCadastroInvalidoEmail() {
         Cadastro pag = PageFactory.initElements(driver, Cadastro.class);
-        pag.preencheDados("studentufjf@gmail.com", "Student Ufjf", "123456","123456");
+        pag.preencheDados("studentufjf@gmail.com","Student Ufjf", "123456", "123456");
 
+        driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[1]/div/div/span/div[5]")).click();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.findElement(By.id("disabled-signup-btn")).click();
+        boolean logado = new WebDriverWait(driver, 10).until(ExpectedConditions.urlMatches(urlLogado));
 
-       // assertTrue(logado);
+        assertFalse(logado);
     }
 
     @Test
-    public void testCadastroInvalidoSenha() throws InterruptedException {
+    public void testCadastroInvalidoSenha() {
         Cadastro pag = PageFactory.initElements(driver, Cadastro.class);
-        pag.preencheDados("studentufjf12@gmail.com", "Student Ufjf", "123456","abcde");
+        pag.preencheDados("studentufjf12@gmail.com","Student Ufjf", "123456Ab", "abcde");
 
+        driver.findElement(By.xpath("/html/body/app/div/main/app-presentation/login-modal/div/div[1]/div/form/div[5]/div/re-captcha/div/div/div")).click();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.findElement(By.id("disabled-signup-btn")).click();
+        boolean logado = new WebDriverWait(driver, 10).until(ExpectedConditions.urlMatches(urlLogado));
 
-        //assertTrue(logado);
+        assertFalse(logado);
     }
+
+	@After
+	public void encerra() {driver.close();}
 }
--- src/test/java/Fase1/ForumTest.java
+++ src/test/java/Fase1/ForumTest.java
@@ -1,5 +1,6 @@
 package Fase1;
 
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -22,10 +23,15 @@ public class ForumTest {
         System.setProperty("webdriver.gecko.driver", "C:\\geckodriver\\geckodriver.exe");
         driver = new FirefoxDriver();
         driver.get("https://atlantis.isti.cnr.it:5000/");
+        //realiza login
         Login pag = PageFactory.initElements(driver, Login.class);
         driver.findElement(By.linkText("Log in")).click();
         pag.preencheDados("teacher@gmail.com", "pass");
         driver.findElement(By.id("log-in-btn")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //acesso ao primeiro curso e a aba forum
+        driver.findElement(By.xpath("/html/body/app/div/main/app-dashboard/div/div[3]/div/div[1]/ul/li[1]/div/div[2]")).click();
+        driver.findElement(By.id("forum-tab-icon")).click();
 
     }
 
@@ -33,18 +39,14 @@ public class ForumTest {
     public void testAcessoForum() throws InterruptedException {
         Forum forum = PageFactory.initElements(driver, Forum.class);
 
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("/html/body/app/div/main/app-dashboard/div/div[3]/div/div[1]/ul/li[1]/div/div[2]")).click();
-
-        driver.findElement(By.id("forum-tab-icon")).click();
         driver.findElement(By.id("add-entry-icon")).click();
 
-        forum.preencheDados("Titulo 3", "Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 ");
+        forum.preencheDados("Titulo 1", "Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 ");
         driver.findElement(By.id("post-modal-btn")).click();
 
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("/html/body/app/div/main/app-course-details/div/div[4]/md-tab-group/div[2]/div[3]/div/ul/div[1]/div/li")).click();
-        driver.findElement(By.xpath("/html/body/app/div/main/app-course-details/div/div[4]/md-tab-group/div[2]/div[3]/div/div/div[1]/div[2]/a")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //não ta achando os elementos abaixo
+        driver.findElement(By.xpath("/html/body/app/div/main/app-course-details/div/div[4]/md-tab-group/div[2]/div[3]/div/ul/div[1]")).click();
+        driver.findElement(By.xpath("/html/body/app/div/main/app-course-details/div/div[4]/md-tab-group/div[2]/div[3]/div/div/div[1]/div[2]/a/i")).click();
 
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         forum.preencheComentarioResposta("Comentário 1123 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1 Comentário 1");
@@ -62,27 +64,27 @@ public class ForumTest {
 
     @Test
     public void testDesativarForum() throws InterruptedException {
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.partialLinkText("Pseudoscientific course")).click();
-        driver.findElement(By.id("forum-tab-icon")).click();
-
         //desativa
         driver.findElement(By.id("edit-forum-icon")).click();
         driver.findElement(By.id("label-forum-checkbox")).click();
         driver.findElement(By.id("put-modal-btn")).click();
 
+        String msgDesativado = driver.findElement(By.xpath("/html/body/app/div/main/app-course-details/div/div[4]/md-tab-group/div[2]/div[3]/div/div/app-error-message/div/h5")).getText();
+        assertEquals("The forum is not activated!",msgDesativado);
+
     }
 
     @Test
     public void testAtivarForum() throws InterruptedException {
-        testDesativarForum();
-        driver.findElement(By.xpath("/html/body/app/div/main/app-dashboard/div/div[3]/div/div[1]/ul/li[1]/div/div[2]")).click();
-        driver.findElement(By.id("forum-tab-icon")).click();
-
         //ativa
         driver.findElement(By.id("edit-forum-icon")).click();
         driver.findElement(By.id("label-forum-checkbox")).click();
         driver.findElement(By.id("put-modal-btn")).click();
+
+        boolean ativado = driver.getPageSource().contains("/html/body/app/div/main/app-course-details/div/div[4]/md-tab-group/div[2]/div[3]/div/ul/div[1]");
+        assertTrue(ativado);
     }
 
+    @After
+    public void encerra() {driver.close();}
 }
